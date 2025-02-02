49e68e1d4b3114dbad79644382b16f1d4a554ffe
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-02-02 21:15:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const ref = {
+  bodyBcg: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
+let timerId = null;
 
+ref.btnStart.addEventListener('click', startRandomBcg);
+ref.btnStop.addEventListener('click', stopRandomBcg);
+
+function startRandomBcg() {
+  timerId = setInterval(() => {
+    ref.bodyBcg.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  ref.btnStart.setAttribute('disabled', 'true');
+  ref.btnStop.removeAttribute('disabled');
+}
+
+function stopRandomBcg() {
+  clearInterval(timerId);
+  ref.btnStop.setAttribute('disabled', 'true');
+  ref.btnStart.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
